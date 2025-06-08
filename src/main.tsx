4f223bbbbae4b15f3c15eb9aa7d2570027a9f2a3
Yash Lad <yashjmlad@gmail.com>
2025-06-08 17:56:43 -0500

    Install Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const system = createSystem(defaultConfig);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
